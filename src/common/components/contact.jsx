0ede39c3dfafd0bcb1fc13474d14e27e34f56334
kilamaelie <elijahkilama14@gmail.com>
2024-01-18 19:17:45 +0300

    contact updated

--- src/common/components/contact.jsx
+++ src/common/components/contact.jsx
@@ -1,12 +1,84 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, TextField, IconButton } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { icons } from '../constant/icons';
+
+const contactForm = [
+	{
+		type: 'email',
+		title: 'Email',
+		placeholder: 'jon@mail.com',
+		required: true,
+	},
+	{
+		type: 'text',
+		title: 'Subject',
+		placeholder: 'Just saying Hey',
+		required: true,
+	},
+	{
+		type: 'text',
+		title: 'Message',
+		placeholder: "Let's talk about ...",
+		rows: 2,
+		multiline: true,
+		required: true,
+	},
+];
 
 export const ContactSection = () => {
 	return (
-		<Grid container>
+		<Grid container direction="column" gap={7}>
 			<Grid item>
 				<Typography align="center" variant="h3">
-					My contact
+					Let&apos;s Connect
 				</Typography>
+			</Grid>
+			<Grid item container spacing={5}>
+				<Grid item xs={12} sm={12} md={6} container direction="column" gap={5}>
+					<Grid item>
+						<Typography variant="body1" color="primary.main">
+							I am currently exploring new opportunities, and my inbox is always
+							open. Whether you have a question or just want to say hi,
+							I&apos;ll do my best to get back to you!
+						</Typography>
+					</Grid>
+					<Grid item container>
+						{icons.map((item) => (
+							<Grid item key={item.id}>
+								<IconButton
+									// LinkComponent={Link}
+									href={item.link}
+									target="_blank"
+									rel="noopener"
+									aria-label={item.name}
+								>
+									{item.icon}
+								</IconButton>
+							</Grid>
+						))}
+					</Grid>
+				</Grid>
+				<Grid item xs={12} sm={12} md={6}>
+					<form>
+						<Grid container direction="row" gap={3}>
+							{contactForm.map((items) => (
+								<Grid item container direction="column" gap={1}>
+									<Grid item>
+										<Typography variant="body2">{items.title}</Typography>
+									</Grid>
+									<Grid item>
+										<TextField size="small" fullWidth {...items} />
+									</Grid>
+								</Grid>
+							))}
+							<Grid item>
+								<LoadingButton fullWidth type="submit" variant="contained">
+									Send message
+								</LoadingButton>
+							</Grid>
+						</Grid>
+					</form>
+				</Grid>
 			</Grid>
 		</Grid>
 	);
--- src/common/components/footer.jsx
+++ src/common/components/footer.jsx
@@ -1,18 +1,25 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 export const Footer = () => {
 	return (
 		<Grid
 			container
 			component="footer"
+			justifyContent="center"
 			xs={11}
 			md={8}
 			xl={6}
 			sx={{
 				marginInline: 'auto',
+				padding: '40px 0px',
 			}}
 		>
-			<Grid item>footer</Grid>
+			<Grid item>
+				<Typography variant="caption" color="primary.light">
+					Copyright &copy; {new Date().getFullYear()} Made with ❤️ by Kilama.
+					All right reserved
+				</Typography>
+			</Grid>
 		</Grid>
 	);
 };
